ae37fcb4d738c8a5a2ae267e1f1cc164e82e576d
Sigi5mund <ajwright124@gmail.com>
2018-01-31 14:53:48 +0000

    repopulated country list on refresh

--- public/index.js
+++ public/index.js
@@ -23,13 +23,25 @@ var populateList = function(){
   })
 }
 
-var populateInformation = function(){
+var displayPopulateInformation = function(previousCountry){
+  var result = document.querySelector('#country-results');
+  var country = previousCountry;
+  result.innerText = `Welcome Back! The FBI have been tracking your activities and they say you were last looking at this country: \n\n Name: ${country.name} \n Population: ${country.population} \n Capital city: ${country.capital}`;
+}
+
+var populateInformation = function(previousCountry){
+  var previousCountry = previousCountry;
   var result = document.querySelector('#country-results');
   var selected = document.querySelector('select');
-  var country = countries[selected.value]
-  result.innerText = `Name: ${country.name} \n Population: ${country.population} \n Capital city: ${country.capital}`
+  var country = countries[selected.value];
+  saveCountry(country);
+  result.innerText = `Name: ${country.name} \n Population: ${country.population} \n Capital city: ${country.capital}`;
 
+}
 
+var saveCountry = function(countryToBeSaved){
+  var jsonString = JSON.stringify(countryToBeSaved);
+  localStorage.setItem('country', jsonString);
 }
 
 var app = function(){
@@ -37,6 +49,9 @@ var app = function(){
   makeRequest(url, requestComplete);
   var dropDown = document.querySelector('select');
   dropDown.addEventListener('change', populateInformation)
+  var savedCountry = JSON.parse(localStorage.getItem('country'));
+
+  displayPopulateInformation(savedCountry);
 }
 
 
